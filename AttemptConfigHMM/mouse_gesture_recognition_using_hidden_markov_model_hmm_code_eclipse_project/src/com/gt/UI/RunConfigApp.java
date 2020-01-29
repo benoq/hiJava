3b7f9ab1dc57d63946395f625f7afc375cd7d16e
Ben <B.grubbauer@gmx.net>
2020-01-29 14:38:34 +0100

    second part (the config app) shold be available for push now;

---
+++ AttemptConfigHMM/mouse_gesture_recognition_using_hidden_markov_model_hmm_code_eclipse_project/src/com/gt/UI/RunConfigApp.java
@@ -0,0 +1,14 @@
+package com.gt.UI;
+
+public class RunConfigApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		ConfigApp configApp = new ConfigApp();
+		
+		configApp.main(args);
+	}
+
+}
---
+++ AttemptConfigHMM/mouse_gesture_recognition_using_hidden_markov_model_hmm_code_eclipse_project/src/module-info.java
@@ -0,0 +1,13 @@
+module q {
+	exports com.gt.db;
+	exports com.gt.hmm.classify.vq;
+	exports com.gt.util;
+	exports test;
+	exports com.gt.gesture.proxy;
+	exports com.gt.UI;
+	exports com.gt.gesture.mouseCapture;
+	exports com.gt.hmm;
+	exports com.gt.gesture.features;
+
+	requires java.desktop;
+}
